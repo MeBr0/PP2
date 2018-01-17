f4b74930197d2d31aa12f5f35da461f10bcbb917
MeBr0 <aza17e@gmail.com>
2018-01-17 14:14:09 +0600

    qwe

---
+++ Random/Random/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Randomm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = Console.ReadLine();
+            string[] b = a.Split();
+            int x = int.Parse(b[0]);
+            int y = int.Parse(b[1]);
+
+            Random r = new Random();
+            int z = r.Next(x, y);
+            Console.WriteLine(z);
+        }
+    }
+}
