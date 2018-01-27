e73f66f6d9297d618ec81940ffc17071450c1dd2
MeBr0 <aza17e@gmail.com>
2018-01-27 21:10:47 +0600

    qwer

---
+++ Lab2/Class/Class/Complex.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class
+{
+    class Complex
+    {
+        int a, b;
+
+        public Complex(int a, int b)
+        {
+            this.a = a;
+            this.b = b;
+        }
+
+        public Complex()
+        {
+
+        }
+
+        int GCD(int x, int y)
+        {
+            x = Math.Abs(x);
+            y = Math.Abs(y);
+            while(x != y)
+            {
+                if (x > y)
+                {
+                    x -= y;
+                }
+                if (y > x)
+                {
+                    y -= x;
+                }
+            }
+            return x;
+        }
+
+        void Simp()
+        {
+            int q = GCD(a, b);
+            a /= q;
+            b /= q;
+        }
+
+        public static Complex operator +(Complex c1, Complex c2)
+        {
+            Complex c = new Complex();
+            c.a = c1.a * c2.b + c2.a * c1.b;
+            c.b = c1.b * c2.b;
+            c.Simp();
+            return c;
+        }
+
+        public static Complex operator -(Complex c1, Complex c2)
+        {
+            Complex c = new Complex();
+            c.a = c1.a * c2.b - c2.a * c1.b;
+            c.b = c1.b * c2.b;
+            c.Simp();
+            return c;
+        }
+
+        public static Complex operator *(Complex c1, Complex c2)
+        {
+            Complex c = new Complex();
+            c.a = c1.a * c2.a;
+            c.b = c1.b * c2.b;
+            c.Simp();
+            return c;
+        }
+
+        public static Complex operator /(Complex c1, Complex c2)
+        {
+            Complex c = new Complex();
+            c.a = c1.a * c2.b;
+            c.b = c1.b * c2.a;
+            c.Simp();
+            return c;
+        }
+        public override string ToString()
+        {
+            if (a == 0) return "0";
+            if (a == b) return "1";
+            if (b == 1) return a + "";
+            return a + "/" + b;
+        }
+    }
+}
---
+++ Lab2/Class/Class/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s1 = Console.ReadLine();
+            string s2 = Console.ReadLine();
+            string[] s3 = s1.Split('/');
+            string[] s4 = s2.Split('/');
+            Complex q = new Complex(int.Parse(s3[0]), int.Parse(s3[1]));
+            Complex w = new Complex(int.Parse(s4[0]), int.Parse(s4[1]));
+            Complex e = new Complex();
+            string s5 = Console.ReadLine();
+            switch (s5)
+            {
+                case "+":
+                    e = q + w;
+                    Console.WriteLine(e);
+                    break;
+                case "-":
+                    e = q - w;
+                    Console.WriteLine(e);
+                    break;
+                case "*":
+                    e = q * w;
+                    Console.WriteLine(e);
+                    break;
+                case "/":
+                    e = q / w;
+                    Console.WriteLine(e);
+                    break;
+                default:
+                    Console.WriteLine("Error!");
+                    break;
+            }
+        }
+    }
+}
