23024800611cec74ed485fe9ca5d65abc93c988e
MeBr0 <aza17e@gmail.com>
2018-01-27 17:49:12 +0600

    qwe

---
+++ Lab1/Classes/Classes/Circle.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    class Circle
+    {
+        double r;
+        double d;
+        double Area;
+        double Circum;
+
+        public Circle(double r)
+        {
+            this.r = r;
+            findArea();
+            findDiam();
+            findCricum();
+        }
+
+        void findDiam()
+        {
+            d = 2 * r;
+        }
+
+        void findArea()
+        {
+            Area = Math.PI * r * r;
+        }
+
+        void findCricum()
+        {
+            Circum = 2 * Math.PI * r;
+        }
+
+        public override string ToString()
+        {
+            return "Area : " + Area + "; Circumference : " + Circum;
+        }
+    }
+}
---
+++ Lab1/Classes/Classes/Rectangle.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    class Rectangle
+    {
+        double a;
+        double b;
+        double Area;
+        double Per;
+
+        public Rectangle(double a, double b)
+        {
+            this.a = a;
+            this.b = b;
+            findArea();
+            findPer();
+        }
+
+        void findArea()
+        {
+            Area = a * b;
+        }
+
+        void findPer()
+        {
+            Per = 2 * (a + b);
+        }
+
+        public override string ToString()
+        {
+            return "Area : " + Area + "; Perimeter : " + Per; 
+        }
+    }
+}
