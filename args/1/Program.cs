6c3939f3d02b57d6abdb1aa5e31f0de6c25ca5aa
MeBr0 <aza17e@gmail.com>
2018-01-27 16:36:00 +0600

    q

---
+++ args/1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace args
+{
+    class Program
+    {
+        static bool IsPrime(string x)
+        {
+            int a = int.Parse(x);
+            for (int i = 2; i <= Math.Sqrt(a); ++i)
+            {
+                if (a % i == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+        static void Main(string[] args)
+        {
+            foreach (string i in args)
+            {
+                if (IsPrime(i))
+                {
+                    Console.Write(i + " ");
+                }
+            }
+        }
+    }
+}
