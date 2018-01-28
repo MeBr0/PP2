a703669be6186f5f8158d129df08a6524b8598c9
MeBr0 <aza17e@gmail.com>
2018-01-28 22:11:12 +0600

    main

--- Lab2/Class/Class/Program.cs
+++ Lab2/Class/Class/Program.cs
@@ -11,13 +11,16 @@ namespace Class
         static void Main(string[] args)
         {
             string s1 = Console.ReadLine();
+            string s5 = Console.ReadLine();
             string s2 = Console.ReadLine();
+
             string[] s3 = s1.Split('/');
             string[] s4 = s2.Split('/');
+
             Complex q = new Complex(int.Parse(s3[0]), int.Parse(s3[1]));
             Complex w = new Complex(int.Parse(s4[0]), int.Parse(s4[1]));
             Complex e = new Complex();
-            string s5 = Console.ReadLine();
+
             switch (s5)
             {
                 case "+":
