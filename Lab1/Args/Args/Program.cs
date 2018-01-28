6d7177e4a394bbe1a850b38b594b8a854219ce4d
MeBr0 <aza17e@gmail.com>
2018-01-28 22:01:54 +0600

    comment

--- Lab1/Args/Args/Program.cs
+++ Lab1/Args/Args/Program.cs
@@ -8,6 +8,11 @@ namespace Args
 {
     class Program
     {
+        /// <summary>
+        /// Function for check whether number prime or not
+        /// </summary>
+        /// <param name="a">input number</param>
+        /// <returns>True if prime, False if not prime</returns>
         static bool IsPrime(int a)
         {
             for(int i=2; i<=Math.Sqrt(a); ++i)
@@ -18,11 +23,11 @@ namespace Args
         }
         static void Main(string[] args)
         {
-            string a = Console.ReadLine();
-            string[] b = a.Split(' ');
-            foreach(string x in b) 
+            string a = Console.ReadLine(); //input string
+            string[] b = a.Split(' ');     //split primary string into array of strings
+            foreach(string x in b)         //go through array b
             {
-                if (IsPrime(int.Parse(x)))
+                if (IsPrime(int.Parse(x))) //if number is prime, then print it
                 {
                     Console.WriteLine(x);
                 }
