7b266d077d6a4188c93ae15e74f0cc94a594c703
MeBr0 <aza17e@gmail.com>
2018-01-28 21:55:18 +0600

    qwe

--- Lab1/Args/Args/Program.cs
+++ Lab1/Args/Args/Program.cs
@@ -10,7 +10,7 @@ namespace Args
     {
         static bool IsPrime(int a)
         {
-            for(int i=0; i<=Math.Sqrt(a); ++i)
+            for(int i=2; i<=Math.Sqrt(a); ++i)
             {
                 if (a % i == 0) return false;
             }
@@ -18,7 +18,9 @@ namespace Args
         }
         static void Main(string[] args)
         {
-            foreach(string x in args)
+            string a = Console.ReadLine();
+            string[] b = a.Split(' ');
+            foreach(string x in b) 
             {
                 if (IsPrime(int.Parse(x)))
                 {
