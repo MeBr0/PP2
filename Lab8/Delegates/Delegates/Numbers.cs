358aa185b15dad92046055cc2213f602dea9d197
MeBr0 <aza17e@gmail.com>
2018-03-18 22:27:45 +0600

    delegates

---
+++ Lab8/Delegates/Delegates/Numbers.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates
+{
+    class Numbers
+    {
+        int a;
+        int b;
+
+        public Numbers(int a, int b)
+        {
+            this.a = a;
+            this.b = b;
+        }
+
+        public int Sum()
+        {
+            return a + b;
+        }
+    }
+}
---
+++ Lab8/Delegates/Delegates/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Delegates
+{
+    class Program
+    {
+        delegate int MyDelegate();
+
+        static void Main(string[] args)
+        {
+            Console.CursorVisible = false;
+
+            string s = Console.ReadLine();
+            string[] q = s.Split(' ');
+
+            int a = int.Parse(q[0]);
+            int b = int.Parse(q[1]);
+
+            Numbers numbers = new Numbers(a, b);
+
+            MyDelegate MD = numbers.Sum;
+
+            Thread.Sleep(1000);
+
+            for(int i = 0; i < 9; ++i)
+            {
+                Console.SetCursorPosition(0, 1);
+                Console.Write(i + 1);
+                Thread.Sleep(1000);
+            }
+
+            Console.SetCursorPosition(0, 1);
+            Console.WriteLine(MD.Invoke());
+        }
+    }
+}
